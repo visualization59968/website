f6eeb52b95aba9a82603af0cde40efe4ba7b4408
mtadjdine@yahoo.com <mtadjdine@yahoo.com>
2019-03-31 12:06:37 -0400

    Added the bubble component and a css file

---
+++ src/Bubble.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+import './Bubble.css' 
+
+
+class bubble extends Component {
+     render () {
+         return (
+             <div> bubbles with the leading causes of death</div>
+         )
+     }
+}
